3f73119c9b54617087b7943f6f982f5be7a25de5
Sophie Messing <sophie.e.messing@gmail.com>
2020-10-26 14:39:31 -0700

    wrote test to test response

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 describe BooksController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "can get books path" do
+    get "/books"
+    must_respond_with :success #success! any response code in the 200 range
+    # must_respond_with :redirect #redirect to another path
+    # must_respond_with :not_found #404
+    # must_respond_with :error #500 server errors
+    # must_respond_with :bad_request
+    # must_respond_with :ok #200
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+
 require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Reporters::JUnitReporter.new]
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
